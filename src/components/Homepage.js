26365e26fd26dc0dda923af7987f1c80e5f074a1
mengxuan <kit.mma@gmail.com>
2020-02-24 19:07:51 -0800

    searchbar first draft

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
+import Searchbar from './Searchbar'
 
 class Homepage extends Component {
+
+  fetchSearchResult = ( query ) => {
+     fetch(`http://localhost:8080/Search?query=${query}`, {
+         method: 'GET',
+         headers: {
+            //  Authorization: `${AUTH_HEADER} ${token}`,
+         },
+     }).then((response) => {
+         if (response.ok) {
+             return response.json();
+         }
+         throw new Error('Failed to load nearby search');
+     }).then((data) => {
+         console.log(data);
+         this.setState( {isLoadingPosts : false, posts: data ? data: []});
+     }).catch((e) => {
+         console.log(e.message);
+         this.setState( {isLoadingPosts: false, error: e.message});
+      });
+  }
+
+    handleSelectChange = ( query ) => {
+        console.log( query )
+        this.fetchSearchResult( query );
+    }
+
   render() {
     return (
         <div>
-          Home
+            <Searchbar handleSelectChange={this.handleSelectChange} />
         </div>
     );
   }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,2 +1,38 @@
-// this is a test commit from mengxuan
-// test two
+import React, {Component} from 'react';
+import { Input } from 'antd';
+import '../styles/Searchbar.css';
+
+const { Search } = Input;
+
+class Searchbar extends Component {
+    constructor( props) {
+        super( props );
+        this.state = {
+            query: '',
+            result: {},
+            loading: false,
+            message: ""
+        }
+    }
+    handleOnInputChange = ( event ) => {
+        const query = event.target.value;
+        this.setState( { query , loading : true, message : ' '})
+        this.props.handleSelectChange( query );
+    }
+
+    render() {
+        const { query } = this.state;
+        return (
+            <div className="searchbar">
+
+                <Search  placeholder="search your dream job here"
+                         value = { query }
+                         onChange={ this.handleOnInputChange }
+                         enterButton />
+
+            </div>
+        );
+    }
+}
+
+export default Searchbar;
