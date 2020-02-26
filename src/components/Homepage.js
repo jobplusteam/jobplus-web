c645cbc77da4ef8aba27a06c8459635929159fb9
mac <mac@macdeMacBook-Pro.local>
2020-02-26 00:30:50 -0800

    update version 2

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import {GEOLOCATION_OPTIONS, POSITION_KEY} from "../constant";
 
 import Searchbar from './Searchbar'
 import Filter from "./Filter"
 import TabContainer from "./TabContainer"
-
+import {jobdata ,jobdata1} from '../constant';
 
 /**
  *   1. data from children: (searchBar and filter need callback function from homepage)
@@ -18,51 +19,103 @@ import TabContainer from "./TabContainer"
  *      c. if user searched, pass searched data to tabContainer
  */
 
-class Homepage extends Component{
-  state = {
-    nearbyJobData: '',
-    recommendJobData: '',
-    searchedJobData: ''
-  }
+class Homepage extends Component {
+    constructor(props) {
+        super(props);
+        this.callAddTab = React.createRef();
+    }
 
-  // fetchSearchResult = ( query ) => {
-  //    fetch(`http://localhost:8080/Search?query=${query}`, {
-  //        method: 'GET',
-  //        headers: {
-  //           //  Authorization: `${AUTH_HEADER} ${token}`,
-  //        },
-  //    }).then((response) => {
-  //        if (response.ok) {
-  //            return response.json();
-  //        }
-  //        throw new Error('Failed to load nearby search');
-  //    }).then((data) => {
-  //        console.log(data);
-  //        this.setState( {isLoadingPosts : false, posts: data ? data: []});
-  //    }).catch((e) => {
-  //        console.log(e.message);
-  //        this.setState( {isLoadingPosts: false, error: e.message});
-  //     });
-  // }
+    state = {
+        loadingGeolocation: false,
+        error: null,
+        nearbyJobData: '',
+        recommendJobData: '',
+        searchedJobData: jobdata1,
+    }
+
+    // fetchSearchResult = ( query ) => {
+    //    fetch(`http://localhost:8080/Search?query=${query}`, {
+    //        method: 'GET',
+    //        headers: {
+    //           //  Authorization: `${AUTH_HEADER} ${token}`,
+    //        },
+    //    }).then((response) => {
+    //        if (response.ok) {
+    //            return response.json();
+    //        }
+    //        throw new Error('Failed to load nearby search');
+    //    }).then((data) => {
+    //        console.log(data);
+    //        this.setState( {isLoadingPosts : false, posts: data ? data: []});
+    //    }).catch((e) => {
+    //        console.log(e.message);
+    //        this.setState( {isLoadingPosts: false, error: e.message});
+    //     });
+    // }
+
+    getGeolocation() {
+        this.setState({
+            loadingGeolocation: true,
+            error: null,
+        });
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onGeolocationSuccess,
+                this.onGeolocationFailure,
+                GEOLOCATION_OPTIONS,
+            );
+        } else {
+            this.setState({
+                loadingGeolocation: false,
+                error: 'Your browser does not support geolocation.',
+            });
+        }
+    }
+
+    onGeolocationSuccess = (position) => {
+        this.setState({
+            loadingGeolocation: false,
+            error: null,
+        })
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem(POSITION_KEY, JSON.stringify({ latitude, longitude }));
+    }
+
+    onGeolocationFailure = () => {
+        this.setState({
+            loadingGeolocation: false,
+            error: 'Failed to load geolocation.',
+        })
+    }
+
+    componentDidMount() {
+        this.getGeolocation();
+    }
+
     // searchBar callback function
     handleSelectChange = (query) => {
-        console.log(query)
-        //this.fetchSearchResult( query );
+        console.log(query);
+        this.setState({
+            searchAction: true
+        });
+        this.callAddTab.current.add(this.state.searchedJobData);
+        //this.fetchSearchResult(query);
     }
     //Filter callback function
     handleFilterChange = (filterKeyWord) => {
-      console.log(filterKeyWord)
+        console.log(filterKeyWord)
     }
 
-  render() {
-    return (
-        <div>
-            <Searchbar handleSelectChange={this.handleSelectChange} />
-            <Filter />
-            <TabContainer />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Searchbar handleSelectChange={this.handleSelectChange}/>
+                <Filter/>
+                <TabContainer ref={this.callAddTab}/>
+            </div>
+        );
+    }
 }
 
 export default Homepage;
