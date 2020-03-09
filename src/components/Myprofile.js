426c1823b177087f1d5f425750a3b360e03923a9
jinglinliang <jinglin@cs.wisc.edu>
2020-03-09 00:57:20 -0500

    update profile to display saved jobs

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
 import logo from '../logo.svg';
 import {Profileview} from './Profileview'
+import {URL_HOST} from "../constant";
 
 class Myprofile extends Component {
+
+  state = {
+    savedJobData: []
+  }
+
+  fetchSavedJobs = () => {
+    let url = `${URL_HOST}/save`;
+    fetch(url, {
+      method: 'GET',
+      credentials: 'include',
+    }).then((response) => {
+      console.log(url);
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      this.setState({savedJobData: data});
+    }).catch((e) => {
+      console.log(e.message);
+    });
+  }
+
+  componentDidMount() {
+    this.fetchSavedJobs();
+  }
+
   render() {
     return (
         <div>
-            <Profileview/>
+            <Profileview jobData={this.state.savedJobData}/>
         </div>
     );
   }
